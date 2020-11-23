9c825fb437fd2d6c35723b61acee30b10cd9d4a0
Naz Sharid <post@nazs.net>
2020-11-23 08:42:00 +0000

    nuxt with modal transition

---
+++ components/oneComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h2 @click="$emit('close')">component one</h2>
+      <p @click="$emit('close2')">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos placeat dolores reprehenderit perferendis eaque. Expedita at hic sapiente odio odit magni nemo, corporis, minima cum nesciunt consequatur ipsum dignissimos explicabo.
+      <p @click="$emit('close3')">another click test</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <h1>welcome to nuxt matebook edition</h1>
+    <one-component @close="test" @close2="test2" @close3="test3" />
+    <two-component />
+    <base-modal @close="hideDialog" :open="dialogIsVisible">
+    <p>This is a test dialog!</p>
+    <button @click="hideDialog">Close it!</button>
+  </base-modal>
+    <div class="container">
+    <button @click="showDialog">Show Dialog</button>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      animatedBlock: false,
+      dialogIsVisible: false,
+      paraIsVisible: false,
+    };
+  },
+  methods: {
+    test() {
+      alert('test 1')
+    },
+    test2() {
+      alert('test 2')
+    },
+    test3() {
+      alert('test 3')
+    },
+    animateBlock() {
+      this.animatedBlock = true;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
+    },
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #810032;
+  border-radius: 30px;
+  background-color: #810032;
+  color: white;
+  cursor: pointer;
+}
+button:hover,
+button:active {
+  background-color: #a80b48;
+  border-color: #a80b48;
+}
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  /* transition: transform 0.3s ease-out; */
+}
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+}
+.animate {
+  /* transform: translateX(-150px); */
+  animation: slide-fade 0.3s ease-out forwards;
+}
+
+.para-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+
+.para-enter-active {
+  animation: slide-scale 0.3s ease-out;
+}
+
+.para-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.para-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.para-leave-active {
+  /* transition: all 0.3s ease-in; */
+  animation: slide-scale 0.3s ease-out;
+}
+
+.para-leave-to {
+  /* opacity: 0;
+  transform: translateY(30px); */
+}
+
+@keyframes slide-scale {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
+}
+</style>
